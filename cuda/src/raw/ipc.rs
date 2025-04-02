f762a81186257d94fec7bef21565e3a78dc8bfeb
Ryohei Kato <r-kato@musen.co.jp>
2025-04-02 19:01:54 +0900

    メモリ関連のモジュールを準備

---
+++ cuda/src/raw/ipc.rs
@@ -0,0 +1,6 @@
+pub unsafe fn open_mem_handle() {}
+pub unsafe fn close_mem_handle() {}
+pub unsafe fn get_mem_handle() {}
+pub unsafe fn open_event_handle() {}
+pub unsafe fn close_event_handle() {}
+pub unsafe fn get_event_handle() {}
