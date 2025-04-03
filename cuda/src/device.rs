d15d22ff42a6c8d06313285fc9f535558e2e13a5
Ryohei Kato <r-kato@musen.co.jp>
2025-04-03 15:56:18 +0900

    デバイス管理を作成

---
+++ cuda/src/device.rs
@@ -0,0 +1,110 @@
+use uuid::Uuid;
+
+use crate::{assert_initialized, error::CudaResult, raw::device::*};
+
+#[derive(Debug, Clone, Copy)]
+pub struct CudaDevice {
+    pub(crate) inner: Device,
+}
+
+impl CudaDevice {
+    pub fn count() -> CudaResult<i32> {
+        assert_initialized!();
+
+        unsafe { get_count() }
+    }
+
+    pub fn new(ordinal: i32) -> CudaResult<Self> {
+        assert_initialized!();
+
+        debug_assert!((0..=Self::count()?).contains(&ordinal));
+        let inner = unsafe { get_device(ordinal) }?;
+        Ok(Self { inner })
+    }
+
+    pub fn name(&self) -> CudaResult<String> {
+        unsafe { get_name(self.inner) }
+    }
+
+    pub fn uuid(&self) -> CudaResult<Uuid> {
+        let c_uuid = unsafe { get_uuid(self.inner) }?;
+        let raw = c_uuid.0.bytes.map(|b| b as u8);
+
+        Ok(Uuid::from_bytes(raw))
+    }
+
+    pub fn total_memory(&self) -> CudaResult<usize> {
+        unsafe { total_mem(self.inner) }
+    }
+
+    pub fn attribute(&self, attr: DeviceAttribute) -> CudaResult<i32> {
+        unsafe { get_attribute(attr, self.inner) }
+    }
+
+    // pub fn default_memory_pool(&self) -> CudaResult<> {}
+
+    pub fn is_affinity_supported(&self) -> CudaResult<bool> {
+        unsafe { get_exec_affinity_support(AffinityType::SmCount, self.inner) }.map(|v| v == 1)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cuda_device_count() {
+        let result = CudaDevice::count();
+        assert!(
+            result.is_ok(),
+            "CUDA device count retrieval failed: {:?}",
+            result
+        );
+    }
+
+    #[test]
+    fn test_cuda_device_new() {
+        let result = CudaDevice::new(0);
+        assert!(result.is_ok(), "CUDA device creation failed: {:?}", result);
+
+        let device = result.unwrap();
+        let name_result = device.name();
+        println!("Device Name: {:?}", name_result);
+        assert!(
+            name_result.is_ok(),
+            "CUDA device name retrieval failed: {:?}",
+            name_result
+        );
+
+        let uuid = device.uuid();
+        println!("Device UUID: {:?}", uuid);
+        assert!(
+            uuid.is_ok(),
+            "CUDA device UUID retrieval failed: {:?}",
+            uuid
+        );
+
+        let total_memory = device.total_memory();
+        println!("Total Memory: {:?}", total_memory);
+        assert!(
+            total_memory.is_ok(),
+            "CUDA device total memory retrieval failed: {:?}",
+            total_memory
+        );
+        let attribute = device.attribute(DeviceAttribute::VirtualAddressManagementSupported);
+        println!("Attribute: {:?}", attribute);
+        assert!(
+            attribute.is_ok(),
+            "CUDA device attribute retrieval failed: {:?}",
+            attribute
+        );
+
+        let affinity = device.is_affinity_supported();
+        println!("Affinity Supported: {:?}", affinity);
+        assert!(
+            affinity.is_ok(),
+            "CUDA device affinity support check failed: {:?}",
+            affinity
+        );
+    }
+}
--- cuda/src/raw/device.rs
+++ cuda/src/raw/device.rs
@@ -3,10 +3,167 @@ use std::mem::MaybeUninit;
 use cuda_sys::ffi as sys;
 
 use crate::error::{CudaResult, ToResult};
-use crate::wrap_sys_handle;
+use crate::{wrap_sys_enum, wrap_sys_handle};
 
 wrap_sys_handle!(Device, sys::CUdevice);
 wrap_sys_handle!(Uuid, sys::CUuuid);
+
+wrap_sys_enum!(
+    DeviceAttribute,
+    sys::CUdevice_attribute,
+    {
+        MaxThreadsPerBlock = CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_BLOCK,
+        MaxBlockDimX = CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_X,
+        MaxBlockDimY = CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_Y,
+        MaxBlockDimZ = CU_DEVICE_ATTRIBUTE_MAX_BLOCK_DIM_Z,
+        MaxGridDimX = CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_X,
+        MaxGridDimY = CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_Y,
+        MaxGridDimZ = CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_Z,
+        SharedMemPerBlock = CU_DEVICE_ATTRIBUTE_SHARED_MEMORY_PER_BLOCK,
+        TotalConstantMemory = CU_DEVICE_ATTRIBUTE_TOTAL_CONSTANT_MEMORY,
+        WarpSize = CU_DEVICE_ATTRIBUTE_WARP_SIZE,
+        MaxPitch = CU_DEVICE_ATTRIBUTE_MAX_PITCH,
+        MaxRegistersPerBlock = CU_DEVICE_ATTRIBUTE_MAX_REGISTERS_PER_BLOCK,
+        ClockRate = CU_DEVICE_ATTRIBUTE_CLOCK_RATE,
+        TextureAlignment = CU_DEVICE_ATTRIBUTE_TEXTURE_ALIGNMENT,
+        GpuOverlap = CU_DEVICE_ATTRIBUTE_GPU_OVERLAP,
+        MultiProcessorCount = CU_DEVICE_ATTRIBUTE_MULTIPROCESSOR_COUNT,
+        KernelExecTimeout = CU_DEVICE_ATTRIBUTE_KERNEL_EXEC_TIMEOUT,
+        Integrated = CU_DEVICE_ATTRIBUTE_INTEGRATED,
+        CanMapHostMemory = CU_DEVICE_ATTRIBUTE_CAN_MAP_HOST_MEMORY,
+        ComputeMode = CU_DEVICE_ATTRIBUTE_COMPUTE_MODE,
+        MaxTexture1DWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_WIDTH,
+        MaxTexture2DWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_WIDTH,
+        MaxTexture2DHeight = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_HEIGHT,
+        MaxTexture3DWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_WIDTH,
+        MaxTexture3DHeight = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_HEIGHT,
+        MaxTexture3DDepth = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_DEPTH,
+        MaxTexture2DLayeredWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_LAYERED_WIDTH,
+        MaxTexture2DLayeredHeight = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_LAYERED_HEIGHT,
+        MaxTexture2DLayeredLayers = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_LAYERED_LAYERS,
+        SurfaceAlignment = CU_DEVICE_ATTRIBUTE_SURFACE_ALIGNMENT,
+        ConcurrentKernels = CU_DEVICE_ATTRIBUTE_CONCURRENT_KERNELS,
+        EccEnabled = CU_DEVICE_ATTRIBUTE_ECC_ENABLED,
+        PciBusId = CU_DEVICE_ATTRIBUTE_PCI_BUS_ID,
+        PciDeviceId = CU_DEVICE_ATTRIBUTE_PCI_DEVICE_ID,
+        TccDriver = CU_DEVICE_ATTRIBUTE_TCC_DRIVER,
+        MemoryClockRate = CU_DEVICE_ATTRIBUTE_MEMORY_CLOCK_RATE,
+        GlobalMemoryBusWidth = CU_DEVICE_ATTRIBUTE_GLOBAL_MEMORY_BUS_WIDTH,
+        L2CacheSize = CU_DEVICE_ATTRIBUTE_L2_CACHE_SIZE,
+        MaxThreadsPerMultiProcessor = CU_DEVICE_ATTRIBUTE_MAX_THREADS_PER_MULTIPROCESSOR,
+        AsyncEngineCount = CU_DEVICE_ATTRIBUTE_ASYNC_ENGINE_COUNT,
+        UnifiedAddressing = CU_DEVICE_ATTRIBUTE_UNIFIED_ADDRESSING,
+        MaxTexture1DLayeredWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_LAYERED_WIDTH,
+        MaxTexture1DLayeredLayers = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_LAYERED_LAYERS,
+        CanTex2DGather = CU_DEVICE_ATTRIBUTE_CAN_TEX2D_GATHER,
+        MaxTexture2DGatherWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_GATHER_WIDTH,
+        MaxTexture2DGatherHeight = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_GATHER_HEIGHT,
+        MaxTexture3DWidthAlt = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_WIDTH_ALTERNATE,
+        MaxTexture3DHeightAlt = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_HEIGHT_ALTERNATE,
+        MaxTexture3DDepthAlt = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE3D_DEPTH_ALTERNATE,
+        PciDomainId = CU_DEVICE_ATTRIBUTE_PCI_DOMAIN_ID,
+        TexturePitchAlignment = CU_DEVICE_ATTRIBUTE_TEXTURE_PITCH_ALIGNMENT,
+        MaxTextureCubemapWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURECUBEMAP_WIDTH,
+        MaxTextureCubemapLayeredWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURECUBEMAP_LAYERED_WIDTH,
+        MaxTextureCubemapLayeredLayers = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURECUBEMAP_LAYERED_LAYERS,
+        MaxSurface1DWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE1D_WIDTH,
+        MaxSurface2DWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE2D_WIDTH,
+        MaxSurface2DHeight = CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE2D_HEIGHT,
+        MaxSurface3DWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE3D_WIDTH,
+        MaxSurface3DHeight = CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE3D_HEIGHT,
+        MaxSurface3DDepth = CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE3D_DEPTH,
+        MaxSurface1DLayeredWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE1D_LAYERED_WIDTH,
+        MaxSurface1DLayeredLayers = CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE1D_LAYERED_LAYERS,
+        MaxSurface2DLayeredWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE2D_LAYERED_WIDTH,
+        MaxSurface2DLayeredHeight = CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE2D_LAYERED_HEIGHT,
+        MaxSurface2DLayeredLayers = CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACE2D_LAYERED_LAYERS,
+        MaxSurfaceCubemapWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACECUBEMAP_WIDTH,
+        MaxSurfaceCubemapLayeredWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACECUBEMAP_LAYERED_WIDTH,
+        MaxSurfaceCubemapLayeredLayers = CU_DEVICE_ATTRIBUTE_MAXIMUM_SURFACECUBEMAP_LAYERED_LAYERS,
+        MaxTexture1DLinearWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_LINEAR_WIDTH,
+        MaxTexture2DLinearWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_LINEAR_WIDTH,
+        MaxTexture2DLinearHeight = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_LINEAR_HEIGHT,
+        MaxTexture2DLinearPitch = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_LINEAR_PITCH,
+        MaxTexture2DMipmappedWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_MIPMAPPED_WIDTH,
+        MaxTexture2DMipmappedHeight = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE2D_MIPMAPPED_HEIGHT,
+        ComputeCapabilityMajor = CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR,
+        ComputeCapabilityMinor = CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR,
+        MaxTexture1DMipmappedWidth = CU_DEVICE_ATTRIBUTE_MAXIMUM_TEXTURE1D_MIPMAPPED_WIDTH,
+        StreamPrioritiesSupported = CU_DEVICE_ATTRIBUTE_STREAM_PRIORITIES_SUPPORTED,
+        GlobalL1CacheSupported = CU_DEVICE_ATTRIBUTE_GLOBAL_L1_CACHE_SUPPORTED,
+        LocalL1CacheSupported = CU_DEVICE_ATTRIBUTE_LOCAL_L1_CACHE_SUPPORTED,
+        MaxSharedMemoryPerMultiprocessor = CU_DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_MULTIPROCESSOR,
+        MaxRegistersPerMultiprocessor = CU_DEVICE_ATTRIBUTE_MAX_REGISTERS_PER_MULTIPROCESSOR,
+        ManagedMemory = CU_DEVICE_ATTRIBUTE_MANAGED_MEMORY,
+        MultiGPuBoard = CU_DEVICE_ATTRIBUTE_MULTI_GPU_BOARD,
+        MultiGPuBoardGroupID = CU_DEVICE_ATTRIBUTE_MULTI_GPU_BOARD_GROUP_ID,
+        HostNativeAtomicSupported = CU_DEVICE_ATTRIBUTE_HOST_NATIVE_ATOMIC_SUPPORTED,
+        SingleToDoublePrecisionPerfRatio = CU_DEVICE_ATTRIBUTE_SINGLE_TO_DOUBLE_PRECISION_PERF_RATIO,
+        PageableMemoryAccess = CU_DEVICE_ATTRIBUTE_PAGEABLE_MEMORY_ACCESS,
+        ConcurrentManagedAccess = CU_DEVICE_ATTRIBUTE_CONCURRENT_MANAGED_ACCESS,
+        ComputePreemptionSupported = CU_DEVICE_ATTRIBUTE_COMPUTE_PREEMPTION_SUPPORTED,
+        CanUseHostPointerForRegisteredMem = CU_DEVICE_ATTRIBUTE_CAN_USE_HOST_POINTER_FOR_REGISTERED_MEM,
+        CanuseStreamMemOpsV1 = CU_DEVICE_ATTRIBUTE_CAN_USE_STREAM_MEM_OPS_V1,
+        CanUse64BitStreamMemOpsV1 = CU_DEVICE_ATTRIBUTE_CAN_USE_64_BIT_STREAM_MEM_OPS_V1,
+        CanUseStreamWaitValueNorV1 = CU_DEVICE_ATTRIBUTE_CAN_USE_STREAM_WAIT_VALUE_NOR_V1,
+        CooperativeLaunch = CU_DEVICE_ATTRIBUTE_COOPERATIVE_LAUNCH,
+        CooperativeMultiDeviceLaunch = CU_DEVICE_ATTRIBUTE_COOPERATIVE_MULTI_DEVICE_LAUNCH,
+        MaxSharedMemoryPerBlockOptin = CU_DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK_OPTIN,
+        CanFlushRemoteWrites = CU_DEVICE_ATTRIBUTE_CAN_FLUSH_REMOTE_WRITES,
+        HostRegisterSupported = CU_DEVICE_ATTRIBUTE_HOST_REGISTER_SUPPORTED,
+        PageableMemoryAccessUsesHostPageTables = CU_DEVICE_ATTRIBUTE_PAGEABLE_MEMORY_ACCESS_USES_HOST_PAGE_TABLES,
+        DirectManagedMemAccessFromHost = CU_DEVICE_ATTRIBUTE_DIRECT_MANAGED_MEM_ACCESS_FROM_HOST,
+        VirtualAddressManagementSupported = CU_DEVICE_ATTRIBUTE_VIRTUAL_ADDRESS_MANAGEMENT_SUPPORTED,
+        HandleTypePosixFileDescriptorSupported = CU_DEVICE_ATTRIBUTE_HANDLE_TYPE_POSIX_FILE_DESCRIPTOR_SUPPORTED,
+        HandleTypeWin32HandleSupported = CU_DEVICE_ATTRIBUTE_HANDLE_TYPE_WIN32_HANDLE_SUPPORTED,
+        HandleTypeWin32KmtHandleSupported = CU_DEVICE_ATTRIBUTE_HANDLE_TYPE_WIN32_KMT_HANDLE_SUPPORTED,
+        MaxBlocksPerMultiprocessor = CU_DEVICE_ATTRIBUTE_MAX_BLOCKS_PER_MULTIPROCESSOR,
+        GenericCompressionSupported = CU_DEVICE_ATTRIBUTE_GENERIC_COMPRESSION_SUPPORTED,
+        MaxPersistingL2CacheSize = CU_DEVICE_ATTRIBUTE_MAX_PERSISTING_L2_CACHE_SIZE,
+        MaxAccessPolicyWindowSize = CU_DEVICE_ATTRIBUTE_MAX_ACCESS_POLICY_WINDOW_SIZE,
+        GpuDirectRdmaWithCudaVmmSupported = CU_DEVICE_ATTRIBUTE_GPU_DIRECT_RDMA_WITH_CUDA_VMM_SUPPORTED,
+        ReservedSharedMemooryPerBlock = CU_DEVICE_ATTRIBUTE_RESERVED_SHARED_MEMORY_PER_BLOCK,
+        SparseCudaArraySupported = CU_DEVICE_ATTRIBUTE_SPARSE_CUDA_ARRAY_SUPPORTED,
+        ReadOnlyHostRegisterSupported = CU_DEVICE_ATTRIBUTE_READ_ONLY_HOST_REGISTER_SUPPORTED,
+        TimelineSemaphoreInteropSupported = CU_DEVICE_ATTRIBUTE_TIMELINE_SEMAPHORE_INTEROP_SUPPORTED,
+        MemoryPoolsSupported = CU_DEVICE_ATTRIBUTE_MEMORY_POOLS_SUPPORTED,
+        GpuDirectRdmaSupported = CU_DEVICE_ATTRIBUTE_GPU_DIRECT_RDMA_SUPPORTED,
+        GpuDirectRdmaFlushWritesOptions = CU_DEVICE_ATTRIBUTE_GPU_DIRECT_RDMA_FLUSH_WRITES_OPTIONS,
+        GpuDirectRdmaWritesOrdering = CU_DEVICE_ATTRIBUTE_GPU_DIRECT_RDMA_WRITES_ORDERING,
+        MempoolSupportedHandleTypes = CU_DEVICE_ATTRIBUTE_MEMPOOL_SUPPORTED_HANDLE_TYPES,
+        ClusterLaunch = CU_DEVICE_ATTRIBUTE_CLUSTER_LAUNCH,
+        DeferredMappingCudaArraySupported = CU_DEVICE_ATTRIBUTE_DEFERRED_MAPPING_CUDA_ARRAY_SUPPORTED,
+        CanUse64BitStreamMemOps = CU_DEVICE_ATTRIBUTE_CAN_USE_64_BIT_STREAM_MEM_OPS,
+        CanUseStreamWaitValueNor = CU_DEVICE_ATTRIBUTE_CAN_USE_STREAM_WAIT_VALUE_NOR,
+        DmaBufSupported = CU_DEVICE_ATTRIBUTE_DMA_BUF_SUPPORTED,
+        IpcEventSupported = CU_DEVICE_ATTRIBUTE_IPC_EVENT_SUPPORTED,
+        MemSyncDomainCount = CU_DEVICE_ATTRIBUTE_MEM_SYNC_DOMAIN_COUNT,
+        TensorMapAccessSupported = CU_DEVICE_ATTRIBUTE_TENSOR_MAP_ACCESS_SUPPORTED,
+        HandleTypeFabricSupported = CU_DEVICE_ATTRIBUTE_HANDLE_TYPE_FABRIC_SUPPORTED,
+        UnifiedFunctionPointers = CU_DEVICE_ATTRIBUTE_UNIFIED_FUNCTION_POINTERS,
+        NumaConfig = CU_DEVICE_ATTRIBUTE_NUMA_CONFIG,
+        NumaId = CU_DEVICE_ATTRIBUTE_NUMA_ID,
+        MulticastSupported = CU_DEVICE_ATTRIBUTE_MULTICAST_SUPPORTED,
+        MpsEnabled = CU_DEVICE_ATTRIBUTE_MPS_ENABLED,
+        HostNumaId = CU_DEVICE_ATTRIBUTE_HOST_NUMA_ID,
+        D3d12CigSupported = CU_DEVICE_ATTRIBUTE_D3D12_CIG_SUPPORTED,
+        MemDecompressAlgorithmMask = CU_DEVICE_ATTRIBUTE_MEM_DECOMPRESS_ALGORITHM_MASK,
+        MemDecompressMaximumLength = CU_DEVICE_ATTRIBUTE_MEM_DECOMPRESS_MAXIMUM_LENGTH,
+        GpuPciDeviceId = CU_DEVICE_ATTRIBUTE_GPU_PCI_DEVICE_ID,
+        GpuPciSubsystemId = CU_DEVICE_ATTRIBUTE_GPU_PCI_SUBSYSTEM_ID,
+        HostNumaMultinodeIpcSupported = CU_DEVICE_ATTRIBUTE_HOST_NUMA_MULTINODE_IPC_SUPPORTED,
+        Max = CU_DEVICE_ATTRIBUTE_MAX,
+    }
+);
+
+wrap_sys_enum!(
+    AffinityType,
+    sys::CUexecAffinityType,
+    {
+        SmCount = CU_EXEC_AFFINITY_TYPE_SM_COUNT,
+        Max = CU_EXEC_AFFINITY_TYPE_MAX,
+    }
+);
 
 pub unsafe fn get_device(ordinal: i32) -> CudaResult<Device> {
     let mut device = 0;
@@ -15,8 +172,12 @@ pub unsafe fn get_device(ordinal: i32) -> CudaResult<Device> {
     Ok(Device(device))
 }
 
-// ToDo. `CU_DEVICE_ATTRIBUTE_*`を作成する
-pub unsafe fn get_attribute() {}
+pub unsafe fn get_attribute(attr: DeviceAttribute, device: Device) -> CudaResult<i32> {
+    let mut value = 0;
+    unsafe { sys::cuDeviceGetAttribute(&mut value, attr.into(), device.0) }.to_result()?;
+
+    Ok(value)
+}
 
 pub unsafe fn get_count() -> CudaResult<i32> {
     let mut count = 0;
@@ -25,7 +186,13 @@ pub unsafe fn get_count() -> CudaResult<i32> {
     Ok(count)
 }
 
-pub unsafe fn get_exec_affinity_support() {}
+pub unsafe fn get_exec_affinity_support(type_: AffinityType, device: Device) -> CudaResult<i32> {
+    let mut value = 0;
+    unsafe { sys::cuDeviceGetExecAffinitySupport(&mut value, type_.into(), device.0) }
+        .to_result()?;
+
+    Ok(value)
+}
 
 #[cfg(target_os = "windows")]
 pub unsafe fn get_luid(device: Device) -> CudaResult<([u8; 8], u32)> {
